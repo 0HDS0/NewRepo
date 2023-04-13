af96eb5355a33985f77606f69726d8cd6698dc2a
hddsrick133@gmail.com <hddsrick133@gmail.com>
2023-04-12 21:08:31 -0300

    Sei lá

---
+++ repos/AULA11/Produto/sript.js
@@ -0,0 +1,21 @@
+function register(){
+
+}
+
+function getProduct(){
+
+}
+
+function loadCategory(){
+    //Por não é necessário alterar a variaveis usamos const
+    const categorySelect = document.getElementById("category")
+    
+    $.ajax({
+        
+    })
+
+}
+
+function listUnexpiredProduct(){
+
+}
